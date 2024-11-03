34bddaa01fc8533a80e7c9f7f0082ef62e3dc270
abhaynm02 <abhaybhaskarnm02@gmail.com>
2024-11-03 14:18:39 +0530

    set up initial spring boot project

---
+++ user-login-registration _/src/test/java/com/abhaynm/user_login_registration/UserLoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhaynm.user_login_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
